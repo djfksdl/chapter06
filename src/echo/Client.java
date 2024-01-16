d923bccf7b206787bee223c8847141b13a6e45a0
djfksdl <djfksdl@gmail.com>
2024-01-16 18:46:57 +0900

    네트워크

---
+++ src/echo/Client.java
@@ -0,0 +1,72 @@
+package echo;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+	public static void main(String[] p) throws IOException {
+		//통신이 목적이라 키자마자 소켓 만들어낼거임
+		Socket socket = new Socket();
+		
+		System.out.println("<클라이언트 시작>");
+		System.out.println("===============");
+		System.out.println("서버에 연결을 요청합니다.");
+		
+		socket.connect(new InetSocketAddress("192.168.0.29",10001));	
+		
+		//서버가 켜져있어야 우리가 주소를 찾을 수 있음
+		System.out.println("서버에 연결되었습니다.");
+		
+		
+		//메세지 내보내기
+		
+/*		OutputStream out = new FileOutputStream("C:\\javaStudy\\MS949-copy.txt");//여기까지는 빨대 꼽은 상태 -> 꼽는순간 안에 내요잉 초기화되어서 같은 파일이름으로 하면 못읽음(일부러 실험한거)
+		OutputStreamWriter osw = new OutputStreamWriter(out,"MS949"); //MS949로 바꿔주세요
+		BufferedWriter bw = new BufferedWriter(osw); 이전에 썼던거랑 비교하려고 놓음*/
+		
+		//메세지 보내기용 스트림
+//		OutputStream out = new FileOutputStream("C:\\javaStudy\\song.txt");//원래 파일은 이렇게 짰었음
+		OutputStream os = socket.getOutputStream(); //주소를 socket한테 내놓으라고함.
+		OutputStreamWriter osw = new OutputStreamWriter(os,"UTF-8"); //out을 osw로 줬다.그래서 os주소를 넣어야함
+		BufferedWriter bw = new BufferedWriter(osw);
+		//이전에 짯던것과 것과 똑같이 쓰면 됨
+		
+		//메세지 받기용 스트림
+		InputStream is = socket.getInputStream();
+		InputStreamReader isw = new InputStreamReader(is ,"UTF-8");
+		BufferedReader br = new BufferedReader(isw);
+		
+		//+키보드 입력
+		Scanner sc = new Scanner(System.in);//외부인 키보드랑 연결하는것
+		String str = sc.nextLine();
+		
+		
+		//메세지 보내기
+//		bw.write("안녕하세요");//write까지 짬
+		bw.write(str);//키보드로 입력한 값을 보내기
+		bw.newLine();//줄바꿈까지 해주기
+		//buffered가 모아서 보내는데 양이 안찰수있다. 안녕하세요가 넘 짧을 수도 있음그래서 억지로 보내라는거 있음
+		bw.flush();//양 안차도 강제로 보내는 역할
+		
+		//메세지 받기용
+		String reMsg = br.readLine();
+		System.out.println("server:[ " + reMsg + " ]");
+		
+		//닫기
+		sc.close();
+		br.close();
+		bw.close();
+		socket.close();
+		
+		
+		
+	}
+}
---
+++ src/echo/Server.java
@@ -0,0 +1,65 @@
+package echo;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+	public static void main(String[] args) throws IOException{  
+
+		//서버소켓생성
+		ServerSocket serverSocket = new ServerSocket();
+		serverSocket.bind(new InetSocketAddress("192.168.0.29", 10001)); //aaa를 ()안에 넣어줌
+//		InetSocketAddress aaa = 주소 어쩌고. ;어차피 한번쓸거라 굳이 이름을 만들지 않는다.
+		
+		//서버시작
+		System.out.println("<서버시작>");
+		System.out.println("==================");
+		System.out.println("[연결을 기다리고 있습니다.]");
+		
+		//클라이언트가 접속을 하면 accept()가 실행됨
+		Socket socket = serverSocket.accept();//위에서 지켜보고있다가 누가 오면 바로 실행 될 수 있도록!  얘네가 소켓 만들어좀. new로 해서 반대쪽과 연결되는!
+//		소켓도 클래스라 필드,메소드,생성자 가지고 있음. 주소를 가지고있음.
+		System.out.println("[클라이언트가 연결되었습니다.]");
+		
+		//in 메세지 받기용 스트림
+		InputStream is = socket.getInputStream(); //어디 짜고 있는지 그림 잘보기
+		InputStreamReader isr = new InputStreamReader(is,"UTF-8");//is를 갖고있고 utf형식이다. ppt 보라색부분
+		BufferedReader br = new BufferedReader(isr);
+		
+		//메세지 보내기용 스트림
+		OutputStream os = socket.getOutputStream();
+		OutputStreamWriter isw = new OutputStreamWriter(os,"UTF-8");//번역기 역할
+		BufferedWriter bw = new BufferedWriter(isw);
+		
+		//메세지 받기
+		String msg =br.readLine();
+		System.out.println("받은 메세지: " + msg);
+		
+		//메세지 보내기
+		bw.write(msg); //받은거 그대로 보내주는 거임. 에코(메아리) 줄바꿈 기호는 들어가지않음. 글씨만 줌
+		bw.newLine(); // 그래서 줄바꿈도 새로 해줘야함. 
+		bw.flush();
+		
+		
+		//닫기
+		
+		bw.close();
+		br.close();
+		socket.close();
+		serverSocket.close();
+		
+		//이게 홈페이지에서 돌아가는 프로그램임
+		
+		
+	}
+
+}
